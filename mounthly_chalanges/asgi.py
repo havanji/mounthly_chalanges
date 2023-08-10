dbd262222804af1c7baefe4ab3675348d8ca43ad
andri <andriyparayko2017@gmail.com>
2023-08-09 18:33:17 -0700

    first commit

---
+++ mounthly_chalanges/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for mounthly_chalanges project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mounthly_chalanges.settings')
+
+application = get_asgi_application()
