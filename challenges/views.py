90e05e7ed60dea730282ef5226c24eb777889dc3
andri <andriyparayko2017@gmail.com>
2023-08-13 19:04:49 -0700

    vid26

--- challenges/views.py
+++ challenges/views.py
@@ -18,7 +18,13 @@ monthly_challenges = {
 }
 
 def monthly_challenge_by_number(request, month):
-    return HttpResponse(month)
+    months = list(monthly_challenges.keys())
+
+    if month > len(months):
+        return HttpResponseNotFound("Invalid month")
+    
+    redirect_month = months[month - 1]
+    return HttpResponseRedirect("/challenges/" + redirect_month)
 
 def monthly_challenge(request, month):
     try:
