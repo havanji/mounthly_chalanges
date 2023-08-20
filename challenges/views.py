070c00b92fe38c9dabaa40652ac076361a17d2d1
andri <andriyparayko2017@gmail.com>
2023-08-19 17:25:12 -0700

    video 36 my not work)

--- challenges/views.py
+++ challenges/views.py
@@ -4,18 +4,18 @@ from django.urls import reverse
 from django.template.loader import render_to_string
 
 monthly_challenges = {
-    "january": "January?, i'm eat no meat for the entire month!",
-    "february": "February?, i'm walk for at least 20 minutes every day!",
-    "march": "March?, i'm learn Django for at least 20 minutes every day!",
-    "april": "April?, i'm eat no meat for the entire month!",
-    "may": "May?, i'm walk for at least 20 minutes every day!",
-    "june": "June?, i'm learn Django for at least 20 minutes every day!",
-    "july": "July?, i'm eat no meat for the entire month!",
-    "august": "August?, i'm walk for at least 20 minutes every day!",
-    "september": "September?, i'm learn Django for at least 20 minutes every day!",
-    "october": "October?, i'm eat no meat for the entire month!",
-    "november": "November?, i'm walk for at least 20 minutes every day!",
-    "december": "December?, i'm learn Django for at least 20 minutes every day!"
+    "january": "January? i'm eat no meat for the entire month!",
+    "february": "February? i'm walk for at least 20 minutes every day!",
+    "march": "March? i'm learn Django for at least 20 minutes every day!",
+    "april": "April? i'm eat no meat for the entire month!",
+    "may": "May? i'm walk for at least 20 minutes every day!",
+    "june": "June? i'm learn Django for at least 20 minutes every day!",
+    "july": "July? i'm eat no meat for the entire month!",
+    "august": "August? i'm walk for at least 20 minutes every day!",
+    "september": "September? i'm learn Django for at least 20 minutes every day!",
+    "october": "October? i'm eat no meat for the entire month!",
+    "november": "November? i'm walk for at least 20 minutes every day!",
+    "december": "December? i'm learn Django for at least 20 minutes every day!"
 }
 
 def index(request):
@@ -43,9 +43,10 @@ def monthly_challenge_by_number(request, month):
 def monthly_challenge(request, month):
     try:
         challenge_text = monthly_challenges[month]
-        return render(request, "challenges/challenge.html")
+        return render(request, "challenges/challenge.html", {
+            "text": challenge_text
+        })
     except:
         return HttpResponseNotFound("<h1>This month is not supported =)</h1>")
     return HttpResponse(challenge_text)
-
 
